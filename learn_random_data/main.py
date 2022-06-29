1e7d10320e64cf13d7d7e3a32a2873beb4d26df7
joshdev1 <32490908+joshdev1@users.noreply.github.com>
2022-06-29 22:11:08 +0100

    extracted model setup functions to own file

--- learn_random_data/main.py
+++ learn_random_data/main.py
@@ -4,44 +4,11 @@ from matplotlib.gridspec import GridSpec
 import seaborn as sns
 import torch
 import torch.nn as nn
+
 from learn_random_data.utils.generate_data import generate_random_data
 from learn_random_data.utils.model_parameters import BATCH_SIZE, DEVICE, DTYPE, NB_INPUTS, NB_STEPS, TIME_STEP,\
     NB_HIDDEN, NB_OUTPUTS, TAU_MEM, TAU_SYN
-
-
-def alpha():
-    return float(np.exp(-TIME_STEP/TAU_SYN))
-
-
-def beta():
-    return float(np.exp(-TIME_STEP/TAU_MEM))
-
-
-def y_data():  # random labels for the random data
-    return torch.tensor(1*(np.random.rand(BATCH_SIZE) < 0.5), device=DEVICE)
-
-
-def weight_scale():
-    return 7*(1.0-beta())  # this should give us some spikes to begin with
-
-
-def initialize_weight_tensor(layer1, layer2):
-    return torch.empty((layer1, layer2),  device=DEVICE, dtype=DTYPE, requires_grad=True)
-
-
-def input_to_hidden_synaptic_weights():
-    w1 = initialize_weight_tensor(NB_INPUTS, NB_HIDDEN)
-    torch.nn.init.normal_(w1, mean=0.0, std=weight_scale()/np.sqrt(NB_INPUTS))
-    return w1
-
-
-def hidden_to_output_synaptic_weights():
-    w2 = initialize_weight_tensor(NB_HIDDEN, NB_OUTPUTS)
-    torch.nn.init.normal_(w2, mean=0.0, std=weight_scale()/np.sqrt(NB_HIDDEN))
-    return w2
-
-
-print("init done")
+from learn_random_data.utils.model_setup import input_to_hidden_synaptic_weights, alpha, beta
 
 
 def multiply_input_spikes_with_weight_matrix():
